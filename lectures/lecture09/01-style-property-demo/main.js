0175d273c232c449ee150f3809fbb1de083287ca
jacksontsaunders <jacksontsaunders@gmail.com>
2022-09-21 15:33:32 -0400

    Added Lecture 10

--- lectures/lecture09/01-style-property-demo/main.js
+++ lectures/lecture09/01-style-property-demo/main.js
@@ -5,9 +5,15 @@ const makeRed = () => {
     document.querySelector('body').style.backgroundColor = 'red';
 };
 
-const makeBlue = () => {
+const makeRainbow = () => {
     // your code here...
     console.log('Change background to blue');
+    document.querySelector('#section1').style.backgroundColor = 'Red'
+    document.querySelector('#section2').style.backgroundColor = 'Orange'
+    document.querySelector('#section3').style.backgroundColor = 'Yellow'
+    document.querySelector('#section4').style.backgroundColor = 'Green'
+    document.querySelector('#section5').style.backgroundColor = 'Blue'
+    document.querySelector('#section6').style.backgroundColor = 'Purple'
 };
 
 const makePink = () => {
