7fef904bf83ea9234505ebba71892cbc3549adfd
jacksontsaunders <jacksontsaunders@gmail.com>
2022-09-26 16:15:30 -0400

    Added rest of alt tags

--- lectures/lecture11/02_function-with-parameters/main.js
+++ lectures/lecture11/02_function-with-parameters/main.js
@@ -1,1 +1,11 @@
 // make a function:
+
+const makeRed = () => {
+    document.querySelector ("body").style.backgroundColor = "red";
+}
+const makeBlue = () => {
+    document.querySelector ("body").style.backgroundColor = "blue";
+}
+const makeGreen = () => {
+    document.querySelector ("body").style.backgroundColor = "green";
+}
