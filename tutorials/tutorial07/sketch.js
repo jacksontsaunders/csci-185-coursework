4c98f930c9046ee86874d1f57bde5df4fb1955dc
jacksontsaunders <jacksontsaunders@gmail.com>
2022-09-28 16:46:23 -0400

    cant figure this part out

--- tutorials/tutorial07/sketch.js
+++ tutorials/tutorial07/sketch.js
@@ -4,16 +4,28 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
 
     // Exercise 1:
-    drawCircle();
+    drawCircle(100, 100, 80, 'hotpink'); // x, y , diameter, color
+    drawCircle(200, 100, 40, 'blue');
+    drawCircle(300, 100, 80, 'teal');
+    drawCircle(400, 100, 40, 'hotpink');
 
     // Exercise 2: 
-    drawOval();
+    drawOval(100, 200, 80, 40, 'teal' );
+    drawOval(200, 200, 40, 80, 'hotpink' );
+    drawOval(300, 200, 80, 40, 'blue' );
+    drawOval(400, 200, 40, 80, 'teal' );
 
     // Exercise 3:
-    drawBullseye();
+    drawBullseye(100, 300, 100, 'lavender', 'black');
+    drawBullseye(200, 300, 50);
+    drawBullseye(300, 300, 100);
+    drawBullseye(400, 300, 50);
 
     // Exercise 4:
-    drawFace();
+    drawFace(100, 400, 100);
+    drawFace(200, 400, 50);
+    drawFace(300, 400, 100);
+    drawFace(400, 400, 50);
 
     drawGrid(canvasWidth, canvasHeight);
 }
@@ -21,38 +33,38 @@ function setup() {
 
 // modify this function so that it accepts and honors
 // the following parameters: centerX, centerY, size, and fillColor
-function drawCircle() {
-    fill('hotpink');
-    circle(100, 100, 100);
+function drawCircle(centerX, centerY, size, fillColor) {
+    fill(fillColor);
+    circle(centerX, centerY, size);
 }
 
 // modify this function so that it accepts and honors
 // the following parameters: centerX, centerY, sizeX, sizeY, and fillColor
-function drawOval() {
-    fill('teal');
-    ellipse(100, 200, 100, 62.5);
+function drawOval(centerX, centerY, sizeX, sizeY, fillColor) {
+    fill(fillColor);
+    ellipse(centerX, centerY, sizeX, sizeY);
 }
 
 
 // modify this function so that it accepts and honors
 // the following 3 parameters: centerX, centerY, and size
-function drawBullseye() {
-    fill('teal');
-    circle(100, 300, 100);
-    fill('navy');
-    circle(100, 300, 75);
-    fill('teal');
-    circle(100, 300, 50);
-    fill('navy');
-    circle(100, 300, 25);
+function drawBullseye(centerX, centerY, size, fillColor1='teal', fillColor2='purple') {
+    fill(fillColor1);
+    circle(centerX, centerY, size);
+    fill(fillColor2);
+    circle(centerX, centerY, size *.75);
+    fill(fillColor1);
+    circle(centerX, centerY, size * .5);
+    fill(fillColor2);
+    circle(centerX, centerY, size * .25);
 }
 
 // modify this function so that it accepts and honors
 // the following 3 parameters: centerX, centerY, and size
-function drawFace() {
-    fill('white');
-    circle(100, 400, 50);
-    fill('black');
-    circle(92, 392, 8);
-    circle(108, 392, 8);
+function drawFace(centerX, centerY, size, backgroundColor='white',eyeColor='black') {
+    fill(backgroundColor);
+    circle(centerX, centerY, size);
+    fill(eyeColor);
+    circle(centerX - 20, centerY - 20, size * .2);
+    circle(centerX + 20, centerY - 20, size * .2);
 }
