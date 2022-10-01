5cc209bc9fa0c22e74f464442a0635cc8f0c0481
jacksontsaunders <jacksontsaunders@gmail.com>
2022-09-30 20:15:08 -0400

    fixed issue

--- tutorials/tutorial07/sketch.js
+++ tutorials/tutorial07/sketch.js
@@ -22,10 +22,11 @@ function setup() {
     drawBullseye(400, 300, 50);
 
     // Exercise 4:
-    drawFace(100, 400, 100);
-    drawFace(200, 400, 50);
-    drawFace(300, 400, 100);
-    drawFace(400, 400, 50);
+    drawFace(100, 400, 50);
+    drawFace(200, 400, 100);
+    drawFace(300, 400, 50);
+    drawFace(400, 400, 100);
+    drawFace(600, 400, 250);
 
     drawGrid(canvasWidth, canvasHeight);
 }
@@ -61,10 +62,43 @@ function drawBullseye(centerX, centerY, size, fillColor1='teal', fillColor2='pur
 
 // modify this function so that it accepts and honors
 // the following 3 parameters: centerX, centerY, and size
-function drawFace(centerX, centerY, size, backgroundColor='white',eyeColor='black') {
-    fill(backgroundColor);
-    circle(centerX, centerY, size);
-    fill(eyeColor);
-    circle(centerX - 20, centerY - 20, size * .2);
-    circle(centerX + 20, centerY - 20, size * .2);
+// function drawFace(centerX, centerY, size, backgroundColor='white',eyeColor='black') {
+//     fill(backgroundColor);
+//     circle(centerX, centerY, size);
+//     fill(eyeColor);
+//     circle(centerX - 20, centerY , size * .2);
+//     circle(centerX + 20, centerY , size * .2);
+// }
+function drawFace(centerX, centerY, size , faceColor = 'yellow') {
+    fill(faceColor);
+    circle(centerX, centerY, size); //100, 400, 50
+    fill('black');
+    let sf = size / 6.5;
+    circle(centerX - sf, centerY - sf, sf); // Left eye
+    circle(centerX + sf, centerY - sf, sf); // Right eye
+
+// make a mouth:
+strokeWeight(size / 25);
+    // line(
+    //     centerX - sf * 2, //x1
+    //     centerY + sf, //y1
+    //     centerX + sf * 2, //x2
+    //     centerY + sf, //y2  
+    // );
+
+    noFill()
+    stroke('black');
+    curve(
+        centerX - sf*2, centerY - 1.5 * size,    // control point
+        centerX - sf*2, centerY + .8 * sf,   // start point
+        centerX + sf*2, centerY + .8 * sf,   // end point
+        centerX + sf*2, centerY - 1.5 * size        // control point
+    );
+
+
+    // noFill();
+    // arc(centerX, centerY, size/2, size/2, .5, -10, OPEN);
 }
+
+
+ 
