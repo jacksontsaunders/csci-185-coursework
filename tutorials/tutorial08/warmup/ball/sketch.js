a60256d7ce4631833ea613a58a0cb7ebbc8b4181
jacksontsaunders <jacksontsaunders@gmail.com>
2022-10-07 16:31:15 -0400

    ball moves between walls

--- tutorials/tutorial08/warmup/ball/sketch.js
+++ tutorials/tutorial08/warmup/ball/sketch.js
@@ -7,21 +7,54 @@ function setup() {
 
 let x = 200;
 let speed = 5;
+let width = 50;
+let color = 'black';
 
 function draw() {
+    // console.log('redrawing the screen');
     // frameRate(4);
-    clear();
+    clear(); //clears circle evertime its drawn
+
+    if (x >= 500 - width/2 ) {
+        // width += 4;
+        // speed = speed * -1; //shortcute below
+        speed *=-1;
+
+    } else if (x <= 150 + width/2 ) {
+        // width += 4;
+        speed *= -1;
+        
+    }
+
 
     // draw walls:
     fill('red');
-    rect(100, 0, 50, canvasHeight);
-    rect(500, 0, 50, canvasHeight);
+    rect(100, 0, 50, canvasHeight); // Left Wall
+    rect(500, 0, 50, canvasHeight); // Right Wall
 
     // draw circle:
-    fill('black');
+    fill(color);
     circle(x, canvasHeight/2, 50);
     x += speed;
-    console.log("x position is:", x);
+    // console.log("x position is:", x);
 
     drawGrid(canvasWidth, canvasHeight);
 }
+
+const changeBallColor = ev => {
+    console.log(ev.code);
+    if (ev.code == 'KeyR') {
+        // console.log ('Change ball to red!');
+        color = 'red';
+    } else if (ev.code == 'KeyP') {
+        color = 'purple';
+    }
+    else if (ev.code == 'Space') {
+        x += 100;
+    }
+
+       // do stuff
+}  
+
+// Add event listener on keydown
+document.addEventListener('keydown', changeBallColor);
