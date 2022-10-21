7551169822cb1df4d694aa076876acb347ffdfc3
jacksontsaunders <jacksontsaunders@gmail.com>
2022-10-21 15:25:35 -0400

    Added creature to tutorial

--- tutorials/tutorial10/your-task/sketch.js
+++ tutorials/tutorial10/your-task/sketch.js
@@ -11,14 +11,27 @@ function setup() {
 }
 
 // replace this function with YOUR creature!
-function drawCreature(x, y, width, primaryColor='hotpink', secondaryColor='black') {
-    fill(primaryColor);
-    stroke(secondaryColor);
-    circle(x, y, width);
 
-    noStroke();
-    fill('black');
-    textSize(16);
-    textAlign(CENTER);
-    text("Replace with your creature", x, y);
+function drawCreature(centerX, centerY, size , faceColor = 'orange',backupColor = 'black') {
+    fill(backupColor)
+    let sf = size / 5;
+     square(centerX-sf * 1 ,centerY-sf * 4,size -sf * 3,) //Stem
+     fill(faceColor);
+     circle(centerX, centerY, size); //100, 400, 50
+     fill(backupColor);
+     
+     triangle(
+         centerX- sf* .5, centerY -sf* 1.3, //top
+         centerX- sf* 1.6, centerY+ sf* .005, //bottom left
+         centerX- sf* .5, centerY+ sf* .001 //bottom right
+         ) // Left eye x1, y1, x2, y2, x3, y3)
+     triangle(
+         centerX+ sf* .5, centerY -sf* 1.3, //top
+         centerX+ sf* 1.6, centerY+ sf* .005, //bottom left
+         centerX+ sf* .5, centerY+ sf* .001 
+         )//bottom right
+     triangle(
+         centerX- sf* .5, centerY+ sf* .6, 
+         centerX+ sf* .5, centerY+ sf* .6 , 
+         centerX, centerY +sf* 1.3) //Nose
 }
