6367d973a034e1f718ee421fd583641c44171827
jacksontsaunders <jacksontsaunders@gmail.com>
2022-11-10 14:24:11 -0500

    Exercise1

--- quiz/quiz02/exercise01/main.js
+++ quiz/quiz02/exercise01/main.js
@@ -1,20 +1,25 @@
 function showFox() {
     // your code here...
     console.log('Change image and paragraph to fox...');
+    document.querySelector("img").src = "images/fox.jpg";
 }
 
 function showLion() {
     // your code here...
     console.log('Change image and paragraph to lion...');
+    document.querySelector("img").src = "images/lion.jpg";
 }
 
 function showTiger() {
     // your code here...
     console.log('Change image and paragraph to tiger...');
+    document.querySelector("img").src = "images/tiger.png";
 }
 
 function showZebra() {
     // your code here...
     console.log('Change image and paragraph to zebra...');
+    document.querySelector("img").src = "images/zebra.jpg";
 }
 
+//
