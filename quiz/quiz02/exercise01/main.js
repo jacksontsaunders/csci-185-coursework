f78be8e4a38158184e7c30f04e294185f1a8eb6c
jacksontsaunders <jacksontsaunders@gmail.com>
2022-11-10 14:42:55 -0500

    Fixed exercise 1

--- quiz/quiz02/exercise01/main.js
+++ quiz/quiz02/exercise01/main.js
@@ -2,24 +2,28 @@ function showFox() {
     // your code here...
     console.log('Change image and paragraph to fox...');
     document.querySelector("img").src = "images/fox.jpg";
+    document.querySelector("p").innerHTML = "Fox";
 }
 
 function showLion() {
     // your code here...
     console.log('Change image and paragraph to lion...');
     document.querySelector("img").src = "images/lion.jpg";
+    document.querySelector("p").innerHTML = "Lion";
 }
 
 function showTiger() {
     // your code here...
     console.log('Change image and paragraph to tiger...');
     document.querySelector("img").src = "images/tiger.png";
+    document.querySelector("p").innerHTML = "Tiger";
 }
 
 function showZebra() {
     // your code here...
     console.log('Change image and paragraph to zebra...');
     document.querySelector("img").src = "images/zebra.jpg";
+    document.querySelector("p").innerHTML = "Zebra";
 }
 
 //
