a23abbc9cc3fb068a404359441c5dde532519a83
jacksontsaunders <jacksontsaunders@gmail.com>
2022-11-10 16:08:34 -0500

    added if statement

--- quiz/quiz02/exercise02/main.js
+++ quiz/quiz02/exercise02/main.js
@@ -5,14 +5,14 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
 
     drawElmo(100, 100, 150, '#0bc9cd', true);   // nose drawn
-    // drawElmo(300, 200, 75, '#0bc9cd', false);   // no nose drawn
-    // drawElmo(100, 325, 100, '#8093f1', false);  // no nose drawn
-    // drawElmo(250, 375, 125, '#7fb285', true);   // nose drawn
-    // drawElmo(550, 200, 250, '#102e4a', true);   // nose drawn
+    drawElmo(300, 200, 75, '#0bc9cd', false);   // no nose drawn
+    drawElmo(100, 325, 100, '#8093f1', false);  // no nose drawn
+    drawElmo(250, 375, 125, '#7fb285', true);   // nose drawn
+    drawElmo(550, 200, 250, '#102e4a', true);   // nose drawn
 
     drawGrid(canvasWidth, canvasHeight);
 }
-function drawElmo(x, y, size, color, hasNose = 'red') {
+function drawElmo(x, y, size, color, hasNose = '#db5461') {
     fill(color);
     circle(x, y, size);
     fill('white');
@@ -21,9 +21,10 @@ function drawElmo(x, y, size, color, hasNose = 'red') {
     fill('black');
     circle(x-20, y-25, size/12);
     circle(x+20, y-25, size/12);
-    fill('#db5461');
-    ellipse(x, y+5, size/4, size/2.9);  // x, y, width, height (but yours needs to scale)
-
+    if (hasNose == true) {
+        fill('#db5461');
+        ellipse(x, y+5, size/4, size/2.9);
+    }
 
 }
 
