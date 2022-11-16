b6c198c48136b22a2f0382879c9a88bc8a6acdf8
jacksontsaunders <jacksontsaunders@gmail.com>
2022-11-16 16:38:17 -0500

    Added artist card and if statement

--- homework/hw05/your_task/main.js
+++ homework/hw05/your_task/main.js
@@ -13,39 +13,75 @@ function search (ev) {
 }
 
 async function getTracks (term) {
-    console.log(`
-        get tracks from spotify based on the search term
-        "${term}" and load them into the #tracks section 
-        of the DOM...`);
+    const tracksEndpoint = baseURL + "?q=" + term + "&type=track";
+    console.log(tracksEndpoint);
+    const data = await fetch(tracksEndpoint).then(response => response.json());
+    console.log(data);
 }
 
 async function getAlbums (term) {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    const albumsEndpoint = baseURL + "?q=" + term + "&type=album";
+    console.log(albumsEndpoint);
+    const data = await fetch(albumsEndpoint).then(response => response.json());
+    console.log(data);
 }
 
 async function getArtist (term) {
-    /*
+    const artistEndpoint = baseURL + "?q=" + term + "&type=artist";
+    console.log(artistEndpoint);
+    
+    const data = await fetch(artistEndpoint).then(response => response.json());
+    
+    // check if there is atleast 1 element in array:
+    if (data.length ===0){
+        document.querySelector('#artist').innerHTML = "No results found."
+        return;
+    }
+    console.log(data);
+    console.log(data[0].name);
+    console.log(data[0].image_url);
+
+    const template = `
+    <img src = "${data[0].image_url}" />
+    <h2>${data[0].image_url}</h2>
+    <p>Popularity: ${data[0].popularity}</p>
+     `;
+
+    const template1 = `
+    <section class="artist-card" id="3Nrfpe0tUJi4K4DXYWgMUX">
+        <div>
+            <img src="${data[0].image_url}">
+            <h2>${data[0].name}</h2>
+            <div class="footer">
+                <a href="${data[0].spotify_url}" target="_blank">
+                    view on spotify
+                </a>
+            </div>
+        </div>
+    </section>`
+
+    console.log(template1);
+
+    document.querySelector('#artist').innerHTML = template1;
+    
+    
+
+     /*
     The Plan:
+    0. Create the URL endpoint based on the term
     1. Go out to spotify and ask for artist info matching the search term
-    (ex. Drake). To do this we need to use the fetch API.
+       (ex. Drake). To do this we need to use the fetch API.
     2. Wait for Spotify to respond to us.
-    3.Once spotify gives us our data, we're going to print it to the console.
-    4. Figure out how to display our artist 
+    3. Once spotify gives us our data, we're going to print it to the console.
+    4. Figure out how to display our artist in a tasteful way
     */
-    console.log(`
-        get artists from spotify based on the search term
-        "${term}" and load the first artist into the #artist section 
-        of the DOM...`);
 };
 
 
 document.querySelector('#search').onkeyup = function (ev) {
     // Number 13 is the "Enter" key on the keyboard
-    console.log(ev);
-    console.log(ev.keyCode);
+    // console.log(ev);
+    // console.log(ev.keyCode);
     if (ev.keyCode === 13) {
         // ev.preventDefault();
         search();
