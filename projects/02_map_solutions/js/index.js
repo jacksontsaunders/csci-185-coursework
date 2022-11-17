bb16cbe6f91ded3212f57d6657ca0feef21d53df
jacksontsaunders <jacksontsaunders@gmail.com>
2022-11-16 20:27:41 -0500

    Added project files

---
+++ projects/02_map_solutions/js/index.js
@@ -0,0 +1,55 @@
+//initialize map:
+const mymap = L.map('mapid').setView([35.14,-82.50], 8);
+
+//initialize tiles (there are many options):
+// L.tileLayer.provider('Stamen.TonerLite').addTo(mymap);
+// L.tileLayer.provider('Stamen.Watercolor').addTo(mymap);
+L.tileLayer.provider('Stamen.Terrain').addTo(mymap);
+// L.tileLayer.provider('Stamen.TerrainBackground').addTo(mymap);
+// L.tileLayer.provider('Stamen.Toner').addTo(mymap);
+// L.tileLayer.provider('Stamen.TonerBackground').addTo(mymap);
+// L.tileLayer.provider('Stamen.TonerHybrid').addTo(mymap);
+// L.tileLayer.provider('Stamen.TonerLines').addTo(mymap);
+// L.tileLayer.provider('Stamen.TonerLabels').addTo(mymap);
+
+
+
+// // CODE TO GENERATE LOTS OF FAKE MARKERS
+// const generateRandomMarkers = (numCoords=100) => {
+//     return new Array(numCoords).fill(null).map(
+//         () => [ (Math.random() * 0.6 + 51), -1 * Math.random() + 0.4 ]
+//     );
+// };
+// const coordinates = generateRandomMarkers(numCoords=50);
+// console.log(coordinates);
+
+// // create markers 
+// for (coord of coordinates) {
+//     const marker = L.marker(coord).addTo(mymap);
+//     marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+// }
+
+const markerWL = L.marker([35.95,-82.51]).addTo(mymap);
+const markerCat = L.marker([35.55,-83.09]).addTo(mymap);
+
+// // create circle:
+// const circle = L.circle([51.5, -0.09], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(mymap);
+
+// // create polygon:
+// const polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(mymap);
+
+// connect each object to a "popup" effect:
+markerWL.bindPopup("<b>Wolf Laurel</b><br>I am a popup.");
+markerCat.bindPopup("<b>Cataloochee</b><br>I am a popup.");
+
+// circle.bindPopup("I am a circle.");
+// polygon.bindPopup("I am a polygon.");
