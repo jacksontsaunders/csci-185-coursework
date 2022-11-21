399d839789b08c3c9788a60ee21ac518a7cb9f74
jacksontsaunders <jacksontsaunders@gmail.com>
2022-11-21 15:58:23 -0500

    got song to display

--- homework/hw05/your_task/main.js
+++ homework/hw05/your_task/main.js
@@ -1,6 +1,6 @@
 const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
 
-function search (ev) {
+function search(ev) {
     const term = document.querySelector('#search').value;
     console.log('search for:', term);
     // issue three Spotify queries at once...
@@ -12,34 +12,39 @@ function search (ev) {
     }
 }
 
-async function getTracks (term) {
+async function getTracks(term) {
     const tracksEndpoint = baseURL + "?q=" + term + "&type=track";
     console.log(tracksEndpoint);
     const data = await fetch(tracksEndpoint).then(response => response.json());
     console.log(data);
+    console.log(data[0]);
+    console.log(data[0].name);
+    console.log(data[0].artist.name);
+    console.log(data[0].album.image_url);
+    document.querySelector('#tracks').innerHTML = data[0].name;
 }
 
-async function getAlbums (term) {
+async function getAlbums(term) {
     const albumsEndpoint = baseURL + "?q=" + term + "&type=album";
-    console.log(albumsEndpoint);
+    // console.log(albumsEndpoint);
     const data = await fetch(albumsEndpoint).then(response => response.json());
-    console.log(data);
+    // console.log(data);
 }
 
-async function getArtist (term) {
+async function getArtist(term) {
     const artistEndpoint = baseURL + "?q=" + term + "&type=artist";
-    console.log(artistEndpoint);
-    
+    // console.log(artistEndpoint);
+
     const data = await fetch(artistEndpoint).then(response => response.json());
-    
+
     // check if there is atleast 1 element in array:
-    if (data.length ===0){
+    if (data.length === 0) {
         document.querySelector('#artist').innerHTML = "No results found."
         return;
     }
-    console.log(data);
-    console.log(data[0].name);
-    console.log(data[0].image_url);
+    // console.log(data);
+    // console.log(data[0].name);
+    // console.log(data[0].image_url);
 
     const template = `
     <img src = "${data[0].image_url}" />
@@ -60,21 +65,21 @@ async function getArtist (term) {
         </div>
     </section>`
 
-    console.log(template1);
+    // console.log(template1);
 
     document.querySelector('#artist').innerHTML = template1;
-    
-    
 
-     /*
-    The Plan:
-    0. Create the URL endpoint based on the term
-    1. Go out to spotify and ask for artist info matching the search term
-       (ex. Drake). To do this we need to use the fetch API.
-    2. Wait for Spotify to respond to us.
-    3. Once spotify gives us our data, we're going to print it to the console.
-    4. Figure out how to display our artist in a tasteful way
-    */
+
+
+    /*
+   The Plan:
+   0. Create the URL endpoint based on the term
+   1. Go out to spotify and ask for artist info matching the search term
+      (ex. Drake). To do this we need to use the fetch API.
+   2. Wait for Spotify to respond to us.
+   3. Once spotify gives us our data, we're going to print it to the console.
+   4. Figure out how to display our artist in a tasteful way
+   */
 };
 
 
